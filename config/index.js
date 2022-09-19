14185e2afa1a44ca5284700fe8bab1818199b5f2
xinxinya512 <112745995+xinxinya512@users.noreply.github.com>
2022-09-19 13:41:04 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "o1uhQ5733ecniG1lWjXtI3lPoSSo",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "LJgOutoKzafS9DsTrtOsUeNHyQDGGEazOAyxY1xBI94",
+      useTemplateId: "Dh0ze1W6uQ97wddIPjpQm8SYnLbNj9vY8s24NOSc2fk",
       // 所在省份
       province: "河南",
       // 所在城市
